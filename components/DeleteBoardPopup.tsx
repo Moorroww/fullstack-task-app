42a36c8dd8aace549a949608c34daa48aefd5595
Norbert <adamusn.kr@gmail.com>
2024-10-07 15:59:30 +0200

    fix deleteAccountPopup rendering

--- components/DeleteBoardPopup.tsx
+++ components/DeleteBoardPopup.tsx
@@ -16,7 +16,7 @@ const DeleteBoardPopup = ({
       onSubmit={() => {
         deleteBoard(selectedBoard.boardID);
       }}
-      className="absolute left-1/2 top-1/2 z-30 w-3/4 max-w-[480px] -translate-x-1/2 -translate-y-1/2 rounded-[6px] bg-foreground p-6 tablet:p-8"
+      className="absolute left-1/2 top-1/2 z-50 w-3/4 max-w-[480px] -translate-x-1/2 -translate-y-1/2 rounded-[6px] bg-foreground p-6 tablet:p-8"
     >
       <h2 className="heading-l text-kbRed">Delete this board?</h2>
       <p className="my-6 text-kbMediumGrey">
